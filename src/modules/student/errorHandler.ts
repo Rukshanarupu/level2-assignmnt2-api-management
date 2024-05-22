84feefb7bfc86a690547bec3fbcc1e1051389172
rukshanarupu <phar.rupu@gmail.com>
2024-05-22 23:51:42 +0600

    validation done

--- src/modules/student/errorHandler.ts
+++
@@ -1,7 +0,0 @@
-import { ValidationError } from 'joi';
-
-export const validateRequest = (data: any, schema: any) => {
-  const { error } = schema.validate(data);
-  if (error) return { error };
-  return { error: null };
-};
